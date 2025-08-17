c55fed099547af35b32e529b494237e6ff331b50
bfoersterling <bjoern.foersterling@gmail.com>
2025-08-17 08:31:39 +0200

    added option to print the version

--- cli_args.go
+++ cli_args.go
@@ -9,6 +9,7 @@ import (
 type cli_args struct {
 	dry_run   bool
 	recursive string
+	version   bool
 }
 
 func get_cli_args() (args cli_args) {
@@ -18,6 +19,8 @@ func get_cli_args() (args cli_args) {
 	flag.StringVar(&args.recursive, "recursive", "", "apply tidynames recursively in the given dir")
 	flag.BoolVar(&args.dry_run, "n", false, "just print changes - do not rename")
 	flag.BoolVar(&args.dry_run, "noop", false, "just print changes - do not rename")
+	flag.BoolVar(&args.version, "v", false, "print the version of tidynames")
+	flag.BoolVar(&args.version, "version", false, "print the version of tidynames")
 	flag.Parse()
 
 	return
@@ -25,6 +28,11 @@ func get_cli_args() (args cli_args) {
 
 func (args cli_args) evalute() (err error) {
 	rc := replace_config{}
+
+	if args.version {
+		fmt.Printf("%s %s, commit: %s, build at: %s.\n", os.Args[0], version, commit, date)
+		os.Exit(0)
+	}
 
 	if len(args.recursive) > 0 {
 		err = fmt.Errorf("%s - Recursive tidying is not implemented yet.\n", os.Args[0])
