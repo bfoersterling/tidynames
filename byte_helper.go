e3d61667bef278cc925137c38190063c6ccd1127
bfoersterling <bjoern.foersterling@gmail.com>
2025-08-23 17:02:11 +0200

    Refactor removal stage (#8)
    * added docs/allowed_range.md
    
    * created function that returns whitelist for removal stage
    
    * implemented removal_stage, but unit tests are still missing
    
    * renamed struct replace_config to tidy_config including its fields
    
    * added unit test for removal_stage
    
    * added test and fixed removal_stage
    
    * added comment
    
    * added test when replacement char is in front of period
    
    * this situation should never arise, because previous stages deal with the situation
    
    * removed unused function

--- byte_helper.go
+++ byte_helper.go
@@ -8,3 +8,19 @@ func peek_byte(raw_bytes []byte, current_index int) byte {
 	}
 	return raw_bytes[current_index+1]
 }
+
+func get_last_byte_from_bytes(raw_bytes []byte) byte {
+	if len(raw_bytes) == 0 {
+		return 0
+	}
+
+	return raw_bytes[len(raw_bytes)-1]
+}
+
+func get_last_rune_from_bytes(raw_bytes []byte) rune {
+	if len(raw_bytes) == 0 {
+		return 0
+	}
+
+	return rune(raw_bytes[len(raw_bytes)-1])
+}
--- tidy.go
+++ tidy.go
@@ -10,39 +10,36 @@ import (
 	"unicode"
 )
 
-type replace_config struct {
-	nonascii   rune
-	whitespace rune
+type tidy_config struct {
+	replacement_char rune
 }
 
-func (rc replace_config) tidy_bytes(name []byte) []byte {
+func (tc tidy_config) tidy_bytes(name []byte) []byte {
 	input_buffer := bytes.NewBuffer(bytes.ToLower(name))
 
-	replace_whitespace(input_buffer, rc.whitespace)
+	replace_whitespace(input_buffer, tc.replacement_char)
 
 	replace_umlauts(input_buffer)
 
-	remove_nonascii(input_buffer)
-
-	remove_special_chars(input_buffer)
+	tc.removal_stage(input_buffer)
 
 	return input_buffer.Bytes()
 }
 
-func (rc replace_config) tidy_string(name string) (proper_name string) {
+func (tc tidy_config) tidy_string(name string) (proper_name string) {
 	for _, r := range name {
 		if (32 < r) && (r < 127) {
 			proper_name += string(r)
 			continue
 		}
 
-		if (r <= 32) && (rc.whitespace != 0) {
-			proper_name += string(rc.whitespace)
+		if (r <= 32) && (tc.replacement_char != 0) {
+			proper_name += string(tc.replacement_char)
 			continue
 		}
 
-		if (r >= 127) && (rc.nonascii != 0) {
-			proper_name += string(rc.nonascii)
+		if (r >= 127) && (tc.replacement_char != 0) {
+			proper_name += string(tc.replacement_char)
 		}
 	}
 
@@ -51,8 +48,8 @@ func (rc replace_config) tidy_string(name string) (proper_name string) {
 	return
 }
 
-func (rc replace_config) tidy_entry(entry_path string, dry_run bool, writer io.Writer) (err error) {
-	new_name := rc.tidy_bytes([]byte(path.Base(entry_path)))
+func (tc tidy_config) tidy_entry(entry_path string, dry_run bool, writer io.Writer) (err error) {
+	new_name := tc.tidy_bytes([]byte(path.Base(entry_path)))
 
 	// trailing slashes will cause path.Base to not return the parent dir
 	entry_path = strings.TrimRight(entry_path, "/")
@@ -83,7 +80,7 @@ func (rc replace_config) tidy_entry(entry_path string, dry_run bool, writer io.W
 	return
 }
 
-func (rc replace_config) tidy_entries(args cli_args, entries []string, writer io.Writer) (err error) {
+func (tc tidy_config) tidy_entries(args cli_args, entries []string, writer io.Writer) (err error) {
 	// this var will only be needed if "." was passed to tidynames
 	var dir_entries []os.DirEntry
 
@@ -102,13 +99,39 @@ func (rc replace_config) tidy_entries(args cli_args, entries []string, writer io
 	}
 
 	for _, arg := range entries {
-		tidy_err := rc.tidy_entry(arg, args.dry_run, os.Stdout)
+		tidy_err := tc.tidy_entry(arg, args.dry_run, os.Stdout)
 		if tidy_err != nil {
 			fmt.Printf("%v", tidy_err)
 		}
 	}
 
 	return
+}
+
+func (tc tidy_config) removal_stage(name *bytes.Buffer) {
+	name_copy := name.String()
+
+	rt := get_whitelist_rt()
+
+	name.Reset()
+
+	for _, r := range name_copy {
+		// don't write replacement char at beginning of name
+		if r == tc.replacement_char && name.Len() == 0 {
+			continue
+		}
+
+		// don't write consecutive replacement chars
+		if (r == tc.replacement_char) &&
+			(get_last_rune_from_bytes(name.Bytes()) == tc.replacement_char) {
+			continue
+		}
+
+		if unicode.In(r, &rt) {
+			name.WriteRune(r)
+			continue
+		}
+	}
 }
 
 // remove characters that are not ascii codes between 32 and 127
@@ -166,8 +189,8 @@ func replace_umlauts(name *bytes.Buffer) {
 	}
 }
 
-// replace whitespace by substitute
-// but do not write consecutive substitute runes
+// replace whitespace by replacement char
+// but do not write consecutive replacement chars
 func replace_whitespace(name *bytes.Buffer, substitute rune) {
 	name_copy := name.Bytes()
 	name.Reset()
